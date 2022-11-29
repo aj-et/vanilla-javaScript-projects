f7999df4f8c12bd05cd5dfb36d119a4cfb764e6d
Aaron Tumbokon <tumbokon.aaronjulius@outlook.com>
2022-11-29 16:42:56 -0700

    Populate UI with saved data

--- 3 - 20 JS Proj Repo/2 - Movie Seat Booking/script.js
+++ 3 - 20 JS Proj Repo/2 - Movie Seat Booking/script.js
@@ -4,6 +4,8 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
+populateUI()
+
 let ticketPrice = +movieSelect.value;
 
 // Save selected movie index and proce
@@ -30,6 +32,24 @@ function updateSelectedCount() {
     total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+// Get data from localstorage and populate UI
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+    
+    if(selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if(selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('selected');
+            }
+        });
+    }
+
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+    if(selectedMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
+}
+
 // Movie select event
 movieSelect.addEventListener('change', e => {
     ticketPrice = +e.target.value;
@@ -47,3 +67,6 @@ container.addEventListener('click', e => {
         updateSelectedCount();
     }
 });
+
+// Initial count and total set
+updateSelectedCount();
