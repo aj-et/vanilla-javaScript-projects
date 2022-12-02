8d933bc84b147911e58aa2c6e1afd4c144664491
tumbokon.aaronjulius@outlook.com <tumbokon.aaronjulius@outlook.com>
2022-12-01 21:16:23 -0700

    Show Millionaires - filter()

--- 3 - 20 JS Proj Repo/05 - DOM Array Methods/script.js
+++ 3 - 20 JS Proj Repo/05 - DOM Array Methods/script.js
@@ -42,6 +42,13 @@ function sortByRichest() {
     updateDOM();
 }
 
+// Filter only millionaires
+function showMillionaires() {
+    data = data.filter(user => user.money > 1000000);
+
+    updateDOM();
+}
+
 // Add new objh to data arr
 function addData(obj) {
     data.push(obj);
@@ -71,5 +78,5 @@ function formatMoney(number) {
 addUserBtn.addEventListener('click', getRandomUser);
 doubleBtn.addEventListener('click', doubleMoney);
 sortBtn.addEventListener('click', sortByRichest);
-showMillionairesBtn.addEventListener('click');
+showMillionairesBtn.addEventListener('click', showMillionaires);
 calculateWealthBtn.addEventListener('click');
