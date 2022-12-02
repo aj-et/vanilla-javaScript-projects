62da14a8a6cdd4062948cf8f8c3395cb36fe28b4
tumbokon.aaronjulius@outlook.com <tumbokon.aaronjulius@outlook.com>
2022-12-01 20:54:23 -0700

    Double Money - map()

--- 3 - 20 JS Proj Repo/05 - DOM Array Methods/script.js
+++ 3 - 20 JS Proj Repo/05 - DOM Array Methods/script.js
@@ -26,6 +26,15 @@ async function getRandomUser() {
     addData(newUser);
 }
 
+// Double everyones money
+function doubleMoney() {
+    data = data.map(user => {
+        return { ...user, money: user.money * 2 };
+    });
+
+    updateDOM();
+}
+
 // Add new objh to data arr
 function addData(obj) {
     data.push(obj);
@@ -53,3 +62,4 @@ function formatMoney(number) {
 
 // Event Listeners
 addUserBtn.addEventListener('click', getRandomUser);
+doubleBtn.addEventListener('click', doubleMoney);
