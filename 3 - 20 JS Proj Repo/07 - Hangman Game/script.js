ba3d3bd74c6b0e10f45250ea0c5c125207913a7a
Aaron Tumbokon <tumbokon.aaronjulius@outlook.com>
2022-12-05 11:00:24 -0700

    Added JS

---
+++ 3 - 20 JS Proj Repo/07 - Hangman Game/script.js
@@ -0,0 +1,10 @@
+const wordEl = document.getElementById('word');
+const wrongLettersEl = document.getElementById('wrong-letters');
+const playAgainBtn = document.getElementById('play-again');
+const popup = document.getElementById('popup-container');
+const notification = document.getElementById('notification-container');
+const finalMessage = document.getElementById('final-message');
+
+const figureParts = document.querySelectorAll('.figure-part');
+
+const words = [];
