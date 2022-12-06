fce1c203641a6238d7aa0ef36a16aabc42c84890
Aaron Tumbokon <tumbokon.aaronjulius@outlook.com>
2022-12-06 16:21:34 -0700

    Display Random Meal & Single Meal CSS

--- 3 - 20 JS Proj Repo/08 - Meal Finder/script.js
+++ 3 - 20 JS Proj Repo/08 - Meal Finder/script.js
@@ -55,6 +55,21 @@ function getMealById(mealID) {
         });
 }
 
+// Fetch random meal from API
+function getRandomMeal() {
+    // Clear meals and heading
+    mealsEl.innerHTML = '';
+    resultHeading.innerHTML = '';
+
+    fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+        .then(res => res.json())
+        .then(data => {
+            const meal = data.meals[0];
+
+            addMealToDOM(meal);
+        });
+}
+
 // Add meal to DOM
 function addMealToDOM(meal) {
     const ingredients = [];
@@ -88,6 +103,7 @@ function addMealToDOM(meal) {
 
 // Event listeners
 submit.addEventListener('submit', searchMeal);
+random.addEventListener('click', getRandomMeal);
 
 mealsEl.addEventListener('click', e => {
     const mealInfo = e.path.find(item => {
