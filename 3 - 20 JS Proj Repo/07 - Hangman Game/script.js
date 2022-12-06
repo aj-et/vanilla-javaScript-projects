e158837f42335dead02127b57fa355de39df3434
Aaron Tumbokon <tumbokon.aaronjulius@outlook.com>
2022-12-06 10:15:41 -0700

    Display Words Function using fetch API and stuff

--- 3 - 20 JS Proj Repo/07 - Hangman Game/script.js
+++ 3 - 20 JS Proj Repo/07 - Hangman Game/script.js
@@ -7,4 +7,48 @@ const finalMessage = document.getElementById('final-message');
 
 const figureParts = document.querySelectorAll('.figure-part');
 
-const words = [];
+// This will fetch a random word each time
+async function getwords() {
+    let words_url = 'https://random-word-api.herokuapp.com/word';
+    try {
+        let response = await fetch(words_url);
+        return await response.json();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+getwords().then(function(result) { // This will return an array of words
+    let selectedWord = result[Math.floor(Math.random() * result.length)]; // This will select a word from the array of words
+    // return selectedWord;
+    console.log(selectedWord);
+
+    let allTheLetters = ['a', 'e', 'i', 'o', 'u', 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'];
+
+    const correctLetters = [];
+    const wrongLetters = [];
+
+    // Show hidden word
+    function displayWord() {
+        wordEl.innerHTML = `
+            ${selectedWord
+                .split('')
+                .map(letter => `
+                    <span class="letter">
+                        ${correctLetters.includes(letter) ? letter : ''}
+                    </span>
+                    `
+                    )
+                    .join('')}
+        `;
+
+        const innerWord = wordEl.innerText.replace(/\n/g, '');
+        
+        if(innerWord === selectedWord) {
+            finalMessage.innerText = 'Congratulations! You won! 😊';
+            popup.style.display = 'flex';
+        }
+    }
+
+    displayWord();
+})
