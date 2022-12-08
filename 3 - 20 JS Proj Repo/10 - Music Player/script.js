7a574d494c20ec32ad83b58b463a4f22de39aedc
Aaron Tumbokon <tumbokon.aaronjulius@outlook.com>
2022-12-08 09:54:36 -0700

    Added next, previous, progress bar functionality

--- 3 - 20 JS Proj Repo/10 - Music Player/script.js
+++ 3 - 20 JS Proj Repo/10 - Music Player/script.js
@@ -36,11 +36,53 @@ function pauseSong() {
     audio.pause();
 }
 
+// Previous Song
+function prevSong() {
+    songIndex--;
+
+    if(songIndex < 0) {
+        songIndex = songs.length - 1;
+    }
+
+    loadSong(songs[songIndex]);
+
+    playSong();
+}
+
+// Next Song
+function nextSong() {
+    songIndex++;
+
+    if(songIndex > songs.length - 1) {
+        songIndex = 0;
+    }
+
+    loadSong(songs[songIndex]);
+
+    playSong();
+}
+
+// Update progress bar
+function updateProgress(e) {
+    const { duration, currentTime } = e.srcElement;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+}
+
 // Update song details
 function loadSong(song) {
     title.innerText = song;
     audio.src = `./music/${song}.mp3`;
     cover.src = `./images/${song}.jpg`;
+}
+
+// Set progress bar
+function setProgress(e) {
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+
+    audio.currentTime = (clickX / width) * duration;
 }
 
 // Event listeners
@@ -53,3 +95,16 @@ playBtn.addEventListener('click', () => {
         playSong();
     }
 });
+
+// Change Song
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+
+// Time/song update
+audio.addEventListener('timeupdate', updateProgress);
+
+// Click on progress bar
+progressContainer.addEventListener('click', setProgress);
+
+// Song ends 
+audio.addEventListener('ended', nextSong);
