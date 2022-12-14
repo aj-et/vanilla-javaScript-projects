82f61edbd601455766da4371174729f6f8a81669
Aaron Tumbokon <tumbokon.aaronjulius@outlook.com>
2022-12-14 09:01:40 -0700

    Edited HTML, CSS, and JS for infinite scroll

--- 3 - 20 JS Proj Repo/11 - Infinite Scroll Posts/script.js
+++ 3 - 20 JS Proj Repo/11 - Infinite Scroll Posts/script.js
@@ -5,4 +5,32 @@ const postsContainer = document.getElementById('posts-container'),
 let limit = 3,
     page = 1;
 
-async
+    // Fetch posts from API
+async function getPosts() {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`);
+
+    const data = await res.json();
+
+    return data;
+}
+
+// Show post and DOM
+async function showPosts() {
+    const posts = await getPosts();
+
+    posts.forEach(post => {
+        const postEl = document.createElement('div');
+        postEl.classList.add('post');
+        postEl.innerHTML = `
+        <div class='number'>${post.id}</div>
+        <div class='post-info'>
+            <h2 class='post-title'>${post.title}</h2>
+            <p class='post-body'>${post.body}</p>
+        </div>
+        `;
+        postsContainer.appendChild(postEl);
+    });
+}
+
+// Show Initial Posts
+showPosts();
