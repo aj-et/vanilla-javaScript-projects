de9dd05b0c760932446117d9b726ac2ed2ae93ec
Aaron Tumbokon <tumbokon.aaronjulius@outlook.com>
2023-01-17 16:38:10 -0700

    Started react and learned about function and class

---
+++ 1 - React Dev Repo/monsters-rolodex/src/App.js
@@ -0,0 +1,29 @@
+import { Component } from 'react';
+
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hello, my name is Aaron
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
