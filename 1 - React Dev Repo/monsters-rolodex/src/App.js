2b0edf45ade551d735d8f57d6589d93b4623dbce
Aaron Tumbokon <tumbokon.aaronjulius@outlook.com>
2023-01-18 16:15:24 -0700

    Initializing Component State. Hardcode to Dynamic

--- 1 - React Dev Repo/monsters-rolodex/src/App.js
+++ 1 - React Dev Repo/monsters-rolodex/src/App.js
@@ -4,22 +4,25 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: 'Aaron', 
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hello, my name is Aaron
+            Hello {this.state.name}
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <button>
+            Change Name
+          </button>
         </header>
       </div>
     );
