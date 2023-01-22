873638b4e9584a1243acfc2ca8f03d96abf4f0aa
tumbokon.aaronjulius@outlook.com <tumbokon.aaronjulius@outlook.com>
2023-01-22 00:18:08 -0700

    Learned about Lifecycle Method : componentDidMount

--- 1 - React Dev Repo/monsters-rolodex/src/App.js
+++ 1 - React Dev Repo/monsters-rolodex/src/App.js
@@ -8,14 +8,20 @@ class App extends Component {
 
     this.state = {
       /* Today we are going to learn first about SPA or (S)ingle (P)age (A)pplications */
-      monsters: [
-        {name: 'Linda', id:'1'},
-        {name: 'Frank', id:'2'},
-        {name: 'Jacky', id:'3'},
-        {name: 'Andrei', id:'4'},
-      ]
-      
+      monsters: [],
     };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState(() => { 
+        return { monsters: users }
+      },
+      () => {
+        console.log(this.state);
+      }
+      ));
   }
 
   render() {
