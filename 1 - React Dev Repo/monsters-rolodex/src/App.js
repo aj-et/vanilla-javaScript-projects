8560d9fc0a7ea77600fc52a84333407fffd07160
Aaron Tumbokon <tumbokon.aaronjulius@outlook.com>
2023-01-23 13:57:13 -0700

    learned about searching using filter functionality

--- 1 - React Dev Repo/monsters-rolodex/src/App.js
+++ 1 - React Dev Repo/monsters-rolodex/src/App.js
@@ -2,6 +2,8 @@ import { Component } from 'react';
 
 import './App.css';
 
+// When creating an app, focus on functionality first then style.
+
 class App extends Component {
   // Constructor runs first
   constructor() {
@@ -11,26 +13,43 @@ class App extends Component {
       /* Today we are going to learn first about SPA or (S)ingle (P)age (A)pplications */
       monsters: [],
     };
+    console.log('constructor');
   }
 
   // componentDidMount runs third
   componentDidMount() {
+    console.log('componentDidMount');
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then((users) => this.setState(() => { 
         return { monsters: users };
       },
       () => {
-        console.log(this.state);
+        // console.log(this.state);
       }
       ));
   }
 
   // Render runs second then re-renders in fourth place
   render() {
-    return <div className="App">
-        { 
-          this.state.monsters.map((monster) => { 
+    console.log('render');
+    return (
+      <div className='App'>
+        <input 
+          className='search-box' 
+          type='search' 
+          placeholder='search monsters' 
+          onChange={(event) => {
+            const searchString = event.target.value.toLocaleLowerCase();
+            const filteredMonsters = this.state.monsters.filter((monster) => { return monster.name.toLocaleLowerCase().includes(searchString); })
+
+            this.setState(() => {
+              return { monsters: filteredMonsters };
+            })
+          // console.log(event.target.value);
+          // this.state.monsters.filter(el => el.includes(event.target.value))
+          }} />
+        { this.state.monsters.map((monster) => { 
           /* What is map()
           
           Google states that map() relies on immutability. Big advantage of map(), is that it creates new array. 
@@ -45,6 +64,7 @@ class App extends Component {
             </div>);
         })}
       </div>
+    );
   }
 }
 
