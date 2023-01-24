04b4cd114c8c1e42558085e71f3d9108ae95d2e8
Aaron Tumbokon <tumbokon.aaronjulius@outlook.com>
2023-01-24 08:57:11 -0700

    Added design and optimized cardlist component

--- 1 - React Dev Repo/monsters-rolodex/src/components/card-list/card-list.component.jsx
+++ 1 - React Dev Repo/monsters-rolodex/src/components/card-list/card-list.component.jsx
@@ -1,16 +1,23 @@
 import { Component } from 'react';
+import './card-list.styles.css';
 
 class CardList extends Component {
     render() {
-        console.log(this.props.monsters);
-        // console.log('render from CardList');
         const { monsters } = this.props;
         // Cannot have multiple parent level components here
         return (
             <div className='card-list'>
-                {monsters.map(monster => (
-                    <h1 key={monster.id}>{monster.name}</h1>
-                ))}
+                {monsters.map(monster => {
+                    const { name, email, id } = monster
+
+                    return (
+                    <div className='card-container' key={id}>
+                        <img alt={`monster ${name}`} 
+                        src={`https://robohash.org/${id}?set=set2&size=180x180`}/>
+                        <h2>{name}</h2>
+                        <p>{email}</p>
+                    </div>
+                )})}
             </div>
         );
     }
