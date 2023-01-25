62c4b3e91e9b35d0bbcef74ce54f89f3fbefd083
Aaron Tumbokon <tumbokon.aaronjulius@outlook.com>
2023-01-25 09:49:00 -0700

    Made app more responsive for smaller devices

--- 1 - React Dev Repo/monsters-rolodex/src/components/card-list/card-list.component.jsx
+++ 1 - React Dev Repo/monsters-rolodex/src/components/card-list/card-list.component.jsx
@@ -11,7 +11,7 @@ class CardList extends Component {
                 {monsters.map((monster) => {
 
                     return (
-                        <Card monster={monster} />
+                            <Card monster={monster} />
                     );
                 })}
             </div>
